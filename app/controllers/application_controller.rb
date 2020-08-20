d5c97ed77e9de43ae7e72be6c1518316afa2bd34
shu082 <s.shu011082@gmail.com>
2020-08-20 16:47:51 +0900

    ユーザー管理機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :encrypted_password, :first_name, :last_name, :katakana_first_name, :katakana_last_name, :birthday])
+  end
+
 end
