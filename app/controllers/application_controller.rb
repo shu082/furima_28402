577ed48746cc78ff90940c02d16c8343469db4f8
shu082 <s.shu011082@gmail.com>
2020-08-21 11:18:29 +0900

    バリデーションの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :encrypted_password, :first_name, :last_name, :katakana_first_name, :katakana_last_name, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :first_name, :last_name, :katakana_first_name, :katakana_last_name, :birthday])
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
+    with_options presence: true do
+       validates :nick_name
+       validates :email, uniqueness: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+       validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+       validates :password, confirmation: true
+       validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+       validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+       validates :katakana_first_name, format:{ with: /\A[ァ-ヶー－]+\z/}
+       validates :katakana_last_name, format:{ with: /\A[ァ-ヶー－]+\z/}
+       validates :birthday
+    end
 end
