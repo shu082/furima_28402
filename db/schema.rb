f9dbde58a601b532f70972d037e947ecb80137b3
shu082 <s.shu011082@gmail.com>
2020-08-25 18:21:11 +0900

    商品一覧表示の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_034354) do
+ActiveRecord::Schema.define(version: 2020_08_25_025550) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", null: false
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2020_08_24_034354) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "items_managements", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_items_managements_on_item_id"
+    t.index ["user_id"], name: "index_items_managements_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -47,4 +56,6 @@ ActiveRecord::Schema.define(version: 2020_08_24_034354) do
   end
 
   add_foreign_key "items", "users"
+  add_foreign_key "items_managements", "items"
+  add_foreign_key "items_managements", "users"
 end
