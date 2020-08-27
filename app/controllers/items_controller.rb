c3e65269c0bd046fdd92efba33f49cdfcfbb790b
shu082 <s.shu011082@gmail.com>
2020-08-27 15:33:57 +0900

    ActiveHashの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
 
+  def created
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image,:name,:description,:category,:status,:delivery_charge,:delivery_source,:delivery_days,:price,:user)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,23 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_charge
+  belongs_to_active_hash :delivery_source
+  belongs_to_active_hash :delivery_day
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category
+    validates :status
+    validates :delivery_charge
+    validates :delivery_source
+    validates :delivery_days
+    validates :price
+  end
   belongs_to :user
   has_one :items_management
+
 end
