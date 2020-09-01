0bda9a4be99e433fa9fc0eae6a1fff8a003d80d2
shu082 <s.shu011082@gmail.com>
2020-09-01 12:31:32 +0900

    'payjp'インストール

--- Gemfile
+++ Gemfile
@@ -67,3 +67,5 @@ gem 'pry-rails'
 group :production do
   gem 'rails_12factor'
 end
+
+gem 'payjp'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_065333) do
+ActiveRecord::Schema.define(version: 2020_08_31_103403) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,20 @@ ActiveRecord::Schema.define(version: 2020_08_27_065333) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "delivery_address", null: false
+    t.string "post_number", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "block", null: false
+    t.string "building_name", null: false
+    t.string "tel", null: false
+    t.bigint "items_management_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["items_management_id"], name: "index_addresses_on_items_management_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -76,6 +90,7 @@ ActiveRecord::Schema.define(version: 2020_08_27_065333) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "addresses", "items_managements"
   add_foreign_key "items", "users"
   add_foreign_key "items_managements", "items"
   add_foreign_key "items_managements", "users"
