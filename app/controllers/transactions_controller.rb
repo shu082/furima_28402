c268480fa28793ea02bba5135ac55a2919ea256b
shu082 <s.shu011082@gmail.com>
2020-09-02 15:05:00 +0900

    クレジット決済機能の実装

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,33 @@
+class TransactionsController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @order = ItemsManagement.new(user_id: order_params[:user_id], item_id: order_params[:item_id])
+
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(:price, :token, :item_id).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
+  end
+end
---
+++ app/javascript/card.js
@@ -0,0 +1,41 @@
+const pay = () => {
+  if (document.URL.match(/transactions/)) {
+    Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+    const form = document.getElementById("charge-form");
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const formResult = document.getElementById("charge-form");
+      const formData = new FormData(formResult);
+      const card = {
+        number: formData.get("number"),
+        cvc: formData.get("cvc"),
+        exp_month: formData.get("exp_month"),
+        exp_year: `20${formData.get("exp_year")}`,
+      };
+
+      Payjp.createToken(card, (status, response) => {
+        if (status === 200) {
+          console.log(status)
+          const token = response.id;
+          const renderDom = document.getElementById("charge-form");
+          const tokenObj = `<input value=${token} type="hidden" name='token'>`;
+          // const price = document.getElementById("price").textContent;
+          // const priceObj = `<input value=${price} type="hidden" name='price'>`;
+          renderDom.insertAdjacentHTML("beforeend", tokenObj);
+          // renderDom.insertAdjacentHTML("beforeend", priceObj);
+        }
+          document.getElementById("card-number").removeAttribute("name");
+          document.getElementById("card-cvc").removeAttribute("name");
+          document.getElementById("card-exp-month").removeAttribute("name");
+          document.getElementById("card-exp-year").removeAttribute("name");
+
+            document.getElementById("charge-form").submit();
+            document.getElementById("charge-form").reset();
+      // else {
+      //     document.getElementById("charge-form").submit();
+      //   }
+      });
+    });
+  };
+};
+window.addEventListener("load", pay);
